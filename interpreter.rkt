14e2fc01ad97c0e7c1a3d2433a094d8525e1e608
dls197 <71739994+dls197@users.noreply.github.com>
2021-03-02 12:55:59 -0500

    Beginning comments
    To be done mostly in order

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,32 +1,41 @@
 #lang racket
 (require "simpleParser.rkt")
+; A simple interpreter for Java
+; 
 
+; interpreter base, to run parser and pass to another function??
 (define interpreter
   (lambda (filename)
     (parser filename)
     ))
 
+; declare variable
 (define var
   (lambda (varlis)
     (cond
       [(null? varlis) varlis])))
 
+; reassign variables
 (define assign
   (lambda (=lis)
     (cond
       [(null? =lis) =lis])))
 
-(define while
-  (lambda (whilelis)
+; print (return as output) variable
+(define return
+  (lambda (returnlis)
     (cond
-      [(null? whilelis) whilelis])))
+      [(null? returnlis) returnlis])))
 
+; if conditional, checks a given conditional in car(cdr) and runs car(cdr(cdr)) if #t or car(cdr(cdr(cdr))) sublist if #f
 (define if
   (lambda (iflis)
     (cond
       [(null? iflis) iflis])))
 
-(define return
-  (lambda (returnlis)
+; if conditional, checks a given conditional in car(cdr) and runs first sublist if #t or second sublist if #f, then it checks the conditional to possibly run itself again
+(define while
+  (lambda (whilelis)
     (cond
-      [(null? returnlis) returnlis])))
+      [(null? whilelis) whilelis])))
+
